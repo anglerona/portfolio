4ad5aae14ea2fc589bc9768e351dd29ac50506df
Angelina <angelina03wu@gmail.com>
2024-09-05 17:09:04 -0400

    aaaaaaaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,26 +80,30 @@ export default function Home() {
           <StarIcon className="size-28 text-purple-300" />
         </StarOrbit>
       {/* </div> */}
+      <div className="relative z-10 flex items-center justify-center h-full">
         <FadeInSection>
-          <div className="py-24">
-            <div className="justify-center w-96 text-center items-center mx-auto">
-              <Image className="rounded-full mx-auto object-fit" src="/goose.png" alt="goose" width={128} height={128} />
-            
-              <div className="px-4 py-8">
-                <h2 className="font-semibold pb-4 text-4xl">Hi, I&apos;m Angelina</h2>
-                <p>I&apos;m a software engineering student @ uWaterloo and I also do freelance design/illustration work. Thanks for stopping by!</p>
-              </div>
-              
+          <div className="text-center max-w-md mx-auto">
+            <Image
+              className="rounded-full mx-auto"
+              src="/goose.png"
+              alt="goose"
+              width={128}
+              height={128}
+            />
+            <div className="px-4 py-8">
+              <h2 className="font-semibold pb-4 text-4xl">Hi, I&apos;m Angelina</h2>
+              <p>
+                I&apos;m a software engineering student @ uWaterloo and I also do
+                freelance design/illustration work. Thanks for stopping by!
+              </p>
             </div>
-            
-            <div className="flex items-center align-middle justify-center">
-              <div className="flex flex-col md:flex-row gap-4 w-full px-4 md:w-auto md:px-0 md:gap-8">
-                <Button href="/dev">Developer Portfolio</Button>
-                <Button href="/design">Design Portfolio</Button>
-              </div>
+            <div className="flex flex-col md:flex-row gap-4 px-4 w-full justify-center">
+              <Button href="/dev">Developer Portfolio</Button>
+              <Button href="/design">Design Portfolio</Button>
             </div>
           </div>
         </FadeInSection>
+      </div>
       
     </div>
   );
