044132e5bf55a73a159d334b157a7fd351860cab
Angelina <angelina03wu@gmail.com>
2024-11-24 00:09:37 -0500

    fixed link issues

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -6,7 +6,7 @@ const BackButton = () => {
   return (
     <FadeInSection>
       <Link
-        href={"/design#projects"}
+        href={"/design"}
         className="flex items-center align-middle px-8 py-4 space-x-2 text-purple-300 hover:text-purple-200 transition-all duration-300 hover:scale-105"
       >
         <Left className="h-3 w-3 fill-current" />
