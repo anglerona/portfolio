35cb7d6aaaf8191b5f96251c4749ff9ea5cd6fab
Angelina <angelina03wu@gmail.com>
2025-08-22 22:28:52 -0400

    updated resume and work history

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -6,6 +6,31 @@ import Badge from "@/components/Badge";
 export function WorkHistory() {
   const data = [
     {
+      title: "May - Aug 2025",
+      content: (
+        <div className="flex flex-col items-start lg:items-center">
+          <Image
+            src="/qualifacts.jpg"
+            alt="Qualifacts Logo"
+            width={400}
+            height={400}
+            className="rounded-lg object-cover h-auto max-w-[200px] md:max-w-[400px] border-8 border-white"
+          />
+          <p className="text-white text-xl font-semibold mt-4 text-left md:text-center">
+            Full Stack Developer Intern
+          </p>
+          <h4 className="text-neutral-300 text-lg font-medium mt-4 mb-2 text-left md:text-center">
+            Tech Stack
+          </h4>
+          <div className="flex flex-wrap gap-2 mt-4">
+            {["React.js", "JavaScript", "Django", "Postman", "Github", "Jira"].map((tech) => (
+              <Badge key={tech} label={tech} variant="outlined" />
+            ))}
+          </div>
+        </div>
+      ),
+    },
+    {
       title: "Sept - Dec 2024",
       content: (
         <div className="flex flex-col items-start lg:items-center">
